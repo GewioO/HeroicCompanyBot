a9eea984c1a07b1279fdd775524581801aed8910
GewioO <68247227+GewioO@users.noreply.github.com>
2020-12-24 16:36:10 +0200

    Add function for save a deck for level
    Add function for check all deck for level

--- bot.py
+++ bot.py
@@ -24,20 +24,58 @@ def write_characters(message):
 @bot.message_handler(content_types=['photo'])
 def photoProcessing(message):
   global recording
-  bot.send_message(message.chat.id, "PHOTO")
   if message.reply_to_message != None:
     i = 0
     while i < len(recording):
       if recording[i] == str(message.reply_to_message.message_id):
-        level_number = message.reply_to_message.text.replace('/write_for', "")
-  
-        document_id = message.photo[0].file_id
-        file_info = bot.get_file(document_id)
-        urllib.request.urlretrieve(f'https://api.telegram.org/file/bot{config.token}/{file_info.file_path}', file_info.file_path)
-        postgresql.db_write_image(level_number,file_info.file_path)
-        bot.send_message(message.chat.id, "!!!  " + file_info.file_path)
+        level_number = message.reply_to_message.text.replace('/write_for', "").strip()
+        if level_number:
+          document_id = message.photo[0].file_id
+          file_info = bot.get_file(document_id)
+          urllib.request.urlretrieve(f'https://api.telegram.org/file/bot{config.token}/{file_info.file_path}', file_info.file_path)
+          result = postgresql.db_write_image(level_number,file_info.file_path)
+          if result:
+            bot.send_message(message.chat.id, "Your deck is recorded")
+          else:
+            bot.send_message(message.chat.id, "Something wrong")
+        else:
+          bot.send_message(message.chat.id, "Something wrong")
       
       i += 1
+
+@bot.message_handler(commands=['level'])
+def level_result(message):
+  ##bot.send_photo(message.from_user.id, photo=open('photos/file_5.jpg', 'rb'))
+  level_number = message.text.replace('/level', "").strip()
+  if level_number:
+    count = level_number.find("all")
+    if count != -1:
+      count = level_number[count:len(level_number)]
+      level_number = level_number.replace(' all', "")
+    else:
+      level_number = digit_check(level_number)
+
+    images = postgresql.db_find_photo(level_number, count)
+    if images:
+      for i in range (0, len(images)):
+        bot.send_photo(message.from_user.id, photo=open(images[i], 'rb'))
+        print("image " + str(i) + " = " + str(images[i]))
+    else:
+      bot.send_message(message.chat.id, "You don't have any images for this level")
+  else:
+    bot.send_message(message.chat.id, "Something wrong")
+  print(str(message.from_user.id))
+
+def digit_check(string):
+  if string.isdigit():
+    return string
+  else:
+    num = ""
+    for char in string:
+      if char.isdigit():
+        num = num + char
+    
+    return num
 
 if __name__ == '__main__':
   bot.infinity_polling(True)
--- lib/postgresql.py
+++ lib/postgresql.py
@@ -14,24 +14,49 @@ def databaseConnection():
 )
 
 def db_write_image(level, path):
-      databaseActions("open")
-      insertNewLevel(level, path)
-      databaseActions("close")
+  databaseActions("open")
+  result = insertNewLevel(level, path)
+  databaseActions("close")
+  return result
+
+def db_find_photo(level, count):
+  databaseActions("open")
+  photo = search_photo(level, count)
+  databaseActions("close")
+  return photo
 
 def insertNewLevel(level, path):
-      cur.execute("SELECT 'heroicStorage' FROM information_schema.tables WHERE table_schema='public'")
-      rows = cur.fetchall()
-      cur.execute("INSERT INTO heroicStorage (LEVEL, PATH, SERIAL_NUMBER) VALUES ("+level+","+path+",'1')")
-      for row in rows:
-            return row[0]
+  cur.execute("SELECT ID, PATH, LEVEL from heroic")
+  number = 1;
+  rows = cur.fetchall()
+  for row in rows:
+    number += 1
+    if row[2] == str(level) and row[1] == path:
+      number = False
+      break
+  if number:
+    cur.execute("INSERT INTO heroic (ID, PATH, LEVEL) VALUES ("
+            +str(number)+",'"+str(path)+"','"+str(level)+"')")
+    return True
+  else:
+    return False
+
+def search_photo(level, count):
+  cur.execute("SELECT ID, PATH, LEVEL from heroic")
+  rows = cur.fetchall()
+  iterator = 0
+  images = []
+  for row in rows:
+    if row[2] == str(level):
+      images.append(row[1])
+  
+  return images
 
 def databaseActions(state):
-      if   state == "open":
-            databaseConnection()
-            global cur
-            cur = con.cursor()
-      elif state == "close":
-            con.commit()  
-            con.close()
-
-db_write_image("1", "path")
+  if   state == "open":
+    databaseConnection()
+    global cur
+    cur = con.cursor()
+  elif state == "close":
+    con.commit()  
+    con.close()
